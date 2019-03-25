cdaa4d5202a9ef422ffd9431f83c07dcf446e12b
Juan David Jaramillo <juandavidjaramillo@MacBookProYungol.local>
2019-03-25 18:27:03 -0500

    subida original de archivos

---
+++ v1/js/funciones.js
@@ -0,0 +1,16 @@
+
+var boton = document.getElementById("boton");
+
+boton.onclick = function(){
+    var a = document.getElementById("numberA").value;
+    var b = document.getElementById("numberB").value;
+    console.log('la variable a es: ',a);
+    console.log('la variable b es: ',b);
+    a = parseInt(a);
+    b = parseInt(b);
+    console.log('y la suma de variables es: ',a+b);
+    var c = a+b;
+
+    document.getElementById("resultado").innerHTML = 'El resultado de la suma es: '+c;
+
+}
