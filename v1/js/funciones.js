17de8bd2f6531b887f13f129387108a0ed64d34d
Juan David Jaramillo <juandavidjaramillo@MacBookProYungol.local>
2019-03-25 19:48:01 -0500

    se arregla error

--- v1/js/funciones.js
+++ v1/js/funciones.js
@@ -1,7 +1,4 @@
-
-var boton = document.getElementById("boton");
-
-boton.onclick = function(){
+document.getElementById("bot1").onclick = function(){
     var a = document.getElementById("numberA").value;
     var b = document.getElementById("numberB").value;
     console.log('la variable a es: ',a);
@@ -10,7 +7,5 @@ boton.onclick = function(){
     b = parseInt(b);
     console.log('y la suma de variables es: ',a+b);
     var c = a+b;
-
     document.getElementById("resultado").innerHTML = 'El resultado de la suma es: '+c;
-
-}
+};
